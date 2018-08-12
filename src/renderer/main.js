637e8adf35224e26a83dfdd4734224a3b0612b30
zpr <zpr@memories.com>
2018-08-12 23:39:31 +0100

    code refactor & format

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,6 +1,6 @@
 import { ipcRenderer } from 'electron';
 import storage from './storage';
-const pkg = require('../../package.json');
+import pkg from '../../package.json';
 
 const init = () => {
   // Set current app version
@@ -12,41 +12,40 @@ const init = () => {
   const downloadBtnElem = document.getElementById("download-btn");
 
   // Get values from local storage
-  const subredditVal = storage.GetValue(subredditElem.id)
-  const limitVal = storage.GetValue(limitElem.id)
+  const subredditVal  = storage.GetValue(subredditElem.id);
+  const limitVal      = storage.GetValue(limitElem.id);
   subredditElem.value = subredditVal ? subredditVal : 'wallpapers';
   limitElem.value     = limitVal ? limitVal : 30;
 
   // Save on local storage on input change
-  const SaveInputValue = e => storage.SetValue(e.srcElement.id, e.srcElement.value);
+  const SaveInputValue  = e => storage.SetValue(e.srcElement.id, e.srcElement.value);
   subredditElem.oninput = SaveInputValue;
   limitElem.oninput     = SaveInputValue;
 
   // When user clicks on download btn
-  downloadBtnElem
-    .onclick = () => {
-      while (logElem.firstChild)
-        logElem.removeChild(logElem.firstChild);
+  downloadBtnElem.onclick = () => {
+    while (logElem.firstChild)
+      logElem.removeChild(logElem.firstChild);
 
-      const downloaderConfig = {
-        reddit: {
-          subreddit: subredditElem.value,
-          totalImages: limitElem.value
-        }
-      };
+    const downloaderConfig = {
+      reddit: {
+        subreddit: subredditElem.value,
+        totalImages: limitElem.value
+      }
+    };
 
-      createLogLine('** Downloading **');
-      ipcRenderer.send("download-items", downloaderConfig);
-    }
-  
+    createLogLine('** Downloading **');
+    ipcRenderer.send("download-items", downloaderConfig);
+  }
+
   // Triggered by the main-process (electron)
   ipcRenderer.on('item-downloaded', (event, filename) => {
     console.log(filename);
-    createLogLine(filename);
+    // createLogLine(filename);
   });
 
-   // Triggered by the main-process (electron)
-   ipcRenderer.on('auto-download', (event, filename) => {
+  // Triggered by the main-process (electron)
+  ipcRenderer.on('auto-download', (event, filename) => {
     console.log('auto-download triggered');
     createLogLine('** auto-download triggered **');
 
