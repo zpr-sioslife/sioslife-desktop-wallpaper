fde410e9d266ae18e5de3624bde05382e886e3e2
zpr-sioslife <joserocha@sioslife.com>
2018-09-19 23:26:03 +0100

    Add react dev tools via "electron-devtools-installer" npm pkg

--- public/electron.js
+++ public/electron.js
@@ -14,8 +14,14 @@ function createWindow() {
   mainWindow.loadURL(process.env.ELECTRON_START_URL || `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
 
-  if (isDev)
+  if (isDev) {
     mainWindow.webContents.openDevTools();
+
+    const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
+    installExtension(REACT_DEVELOPER_TOOLS)
+      .then(name => console.log(`Added Extension:  ${name}`))
+      .catch(err => console.log('An error occurred: ', err));
+  }
 }
 
 app.on('ready', createWindow);
